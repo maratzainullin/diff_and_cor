f00e444c9331d701744e98fc647967e026399fd9
Марат Зайнуллин <maratzainullin@MacBook-Pro-Marat.local>
2020-02-21 23:39:17 +0300

    coords table dont work anyway

---
+++ diff_and_cor/par_run.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+#SBATCH -J diff700
+#SBATCH --exclusive
+#SBATCH --comment t700l12Time5*10e4ps
+#SBATCH -p RT
+#SBATCH -N 3
+#SBATCH -n 48
+#SBATCH
+mpirun -np 46 ~/tools/lammps/src/lmp_mpi -in diff.lmp -var l 2.8712 -var t 700 &
+mpirun -np 2 ~/tools/lammps/src/lmp_mpi -in diff.lmp -l none -var l 2.8712 -var t 700
